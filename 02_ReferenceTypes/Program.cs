ce9acb5e38816d52fe28717aae365b32313906e2
TeliciaShelton <104474831+TeliciaShelton@users.noreply.github.com>
2022-05-14 15:53:59 -0500

    143 notes

---
+++ 02_ReferenceTypes/Program.cs
@@ -0,0 +1,79 @@
+﻿/*
+!Value Types vs Reference Types
+    -Value Types: have a specific size and stored in stack memory, 
+    -Reference Types: Have a variable size, and store in heap memory
+
+*/
+//! Strings
+string someString; 
+string initialized = "This is initialized";
+
+string firstName = "Jill";
+string lastName = "Schmoe";
+
+string fullName = firstName + ' ' + lastName;
+System.Console.WriteLine(fullName);
+
+//* interpolation 
+string interpolatedFullName = $"{firstName} {lastName}";
+System.Console.WriteLine(interpolatedFullName);
+
+/*
+!Collections
+    A string is ttechnically a collection (of character)
+*/
+//*Array 
+char firstLetter = firstName[0];
+System.Console.WriteLine(firstLetter);
+string extraItem = "another string";
+
+//
+string[] stringArray = {"Hello", "World", "Why", "is it","always", "hello world?", extraItem};
+System.Console.WriteLine(stringArray[1]);
+
+/*
+    We define the type with the square brackets. The curly brackets will detail the information within that array.  Array do have a fixed size determined on how they are created. 
+
+    Targeting index within an array, we alway start with position zero. 
+*/
+
+System.Console.WriteLine(stringArray[6]);
+int lengthValue = stringArray. Length;
+System.Console.WriteLine(lengthValue);
+System.Console.WriteLine(stringArray[stringArray.Length - 1]);
+
+//* List 
+List<string> listofstrings = new List<string>();
+listofstrings.Add("Hello");
+listofstrings.Add("World");
+/*
+    A list is part of a class that is built into C# which allows us various methods to manipulate it.
+*/
+
+List<int> listofInts;
+/*
+*Queue 
+    -First in/First out
+*/
+Queue<string> firstInfirstOut = new Queue<string>();
+firstInfirstOut.Enqueue("Im first");
+firstInfirstOut.Enqueue("Im next");
+System.Console.WriteLine(firstInfirstOut.Peek()); //Peek() is a method that allows us to view what item is first is queue.
+
+firstInfirstOut.Dequeue();
+System.Console.WriteLine(firstInfirstOut.Peek());
+
+/*
+*Dictionaries
+    -A collection where we are assigning a key to a value pair.
+*/
+Dictionary<int,string> keyAndValue = new Dictionary<int, string>();
+
+keyAndValue.Add(21, "legal, drinking age");
+keyAndValue.Add(18, "legal, voting age");
+keyAndValue.Add(19, "legal, voting age");
+System.Console.WriteLine(keyAndValue[21]); //We can target a key just like we target an index vaule in an array. 
+// The values can be the same; however you cannot use the same key for different values.
+
+Dictionary<string, string> stringDictionary = new Dictionary<string, string>();
+stringDictionary.Add("Triangle", "the set of all points in a plae defined by the area within three points.");
---
+++ 03_Operators/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+! Arithmetic Operators 
+    Math and logic operators
+*/
+int a = 10;
+int b = 2;
+System.Console.WriteLine(a+b);
+
+int difference = a - b; 
+System.Console.WriteLine(difference);
+System.Console.WriteLine(a * b);
+System.Console.WriteLine(a / b);
+
+int numberA = 22;
+int numberB= 15;
+
+System.Console.WriteLine(numberA / numberB); //Provides an incorrect value because the return is an "int".
+System.Console.WriteLine((decimal) numberA / (decimal) numberB);
+System.Console.WriteLine(numberA % numberB); //<pdi;is - the remainderof what has been divided
+
+DateTime now = DateTime.Now;
+DateTime someDay = new DateTime(1985, 10, 26);
+TimeSpan timeSpan = now - someDay; 
+System.Console.WriteLine(timeSpan / 365);
+
+/*
+! Comparison Operators
+    Compare two different values in a certain way. 
+*/
+string name = "Peter";
+string anotherName = "Peter";
+System.Console.WriteLine(name == "Peter");
+System.Console.WriteLine(name == anotherName);
+
+int age = 30;
+bool notEqual = age != 123;
+System.Console.WriteLine(notEqual);
+
+List<string> firstList = new List<string>();
+List<string> secondList = new List<string>();
+
+firstList.Add(name);
+secondList.Add(name);
+
+System.Console.WriteLine(firstList == secondList);
+// Returns false because they have different addresses in memory. If you were to compare something like this, you would need to compare the exact items within the list (done with a loop).
+
+bool greaterThan = age > 12;
+bool greaterthanOrEqual= age >= 18;
+bool lessThan = age < 100;
+bool lessThanOrEqual = age <= 20;
+
+//* OR  
+bool orValue = lessThan || lessThanOrEqual;
+System.Console.WriteLine($"OR: {orValue}");
+
+//* AND
+int x =3; 
+bool andValue = x < 5 && greaterThan;
+System.Console.WriteLine($"AND: {andValue}");
+
+if (x > 5 && x > 2 || x < 50)
+{
+    System.Console.WriteLine("Yup");
+}
+else
+{
+    System.Console.WriteLine("Nope");
+}
